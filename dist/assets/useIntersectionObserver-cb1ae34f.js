c668d5f189911ffed11350eb62ac6754d8958225
sangpok <48979587+sangpok@users.noreply.github.com>
2023-08-02 15:03:32 +0900

    build

--- dist/assets/useIntersectionObserver-cb1ae34f.js
+++
@@ -1,1 +0,0 @@
-import{t as r}from"./index-92835960.js";function b(t){const s=r.useRef(new IntersectionObserver(e=>{e.forEach(c=>{c.isIntersecting&&t()})},{threshold:.1})),n=r.useCallback(e=>{s.current.observe(e)},[]),o=r.useCallback(e=>{s.current.unobserve(e)},[]);return[n,o]}export{b as u};
